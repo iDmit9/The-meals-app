db38d3149d3a76da5c3fdb30540a85d0e29e6efb
iDmitriy-dev <dmitriy.p86@ya.ru>
2019-10-07 23:53:25 +0600

    categories created

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,37 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { Platform } from 'react-native';
+
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
+import Colors from '../constants/Colors';
+
+const MealsNavigator = createStackNavigator({
+    Categories: {
+        screen: CategoriesScreen,
+        navigationOptions: {
+            headerTitle: 'Meal Categories'
+        }
+    },
+    CategoryMeals: {
+        screen: CategoryMealsScreen,
+        navigationOptions: {
+            // headerStyle: {
+            //     backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+            // },
+            // headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+        }
+    },
+    MealDetail: MealDetailScreen
+}, {
+    // mode: 'modal',
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    }
+});
+
+export default createAppContainer(MealsNavigator);
---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+
+const CategoryMealScreen = props => {
+    const renderMealItem = itemData => {
+        return (
+            <View><Text>{itemData.item.title}</Text></View>
+        )
+    };
+
+    const catId = props.navigation.getParam('categoryId');
+
+    const displayedMeals = MEALS.filter(
+        meal => meal.categoryIds.indexOf(catId) >= 0
+    );
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+    return (
+        <View style={styles.screen}>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem}
+            />
+        </View>
+    );
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+    const catId = navigationData.navigation.getParam('categoryId')
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+    return {
+        headerTitle: selectedCategory.title
+    };
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default CategoryMealScreen;
